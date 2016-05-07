5772e112ade44c2921307faed136d2cfe08fc77e
T Y <T Y>
2016-05-07 14:49:14 +0800

    four

--- app/src/main/java/user/UserRegister.java
+++ app/src/main/java/user/UserRegister.java
@@ -2,7 +2,10 @@ package user;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,7 +13,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.ytw.note.MainActivity;
 import com.example.ytw.note.R;
+
+import org.json.JSONObject;
+
+import uitl.HttpUtil;
 
 /**
  * Created by YTW on 2016/5/6.
@@ -26,6 +34,10 @@ public class UserRegister extends AppCompatActivity {
     private Button mSubmitRegis;
     private boolean canRegister;
 
+    //用于对服务器返回的信息做判断
+    public static final int CODE_SUCCESS = 0;   //成功
+    public static final int CODE_FAILE = 1;     //失败
+    public static final int CODE_NOTALL = 2;    //数据不全
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,23 +50,91 @@ public class UserRegister extends AppCompatActivity {
         mSubmitRegis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String strNumber = mUserNumber.getText().toString();
-                String strName = mUserName.getText().toString();
-                String strPwd = mUserPwd.getText().toString();
+                final String strNumber = mUserNumber.getText().toString();
+                final String strName = mUserName.getText().toString();
+                final String strPwd = mUserPwd.getText().toString();
                 String strRePwd = mUserRePwd.getText().toString();
 
                 //对用户输入的信息进行判断
                 canRegister = isCanRegister(strNumber, strName, strPwd, strRePwd);
 
                 if (canRegister){
-                    //如果完成注册，将信息上传服务器
-
+                    //如果信息无误，将信息上传服务器，完成注册
+                    new Thread(){
+                        @Override
+                        public void run() {
+                            submitUserInfo(strNumber,strName, strPwd);
+                        }
+                    }.start();
                 }
-
             }
         });
 
     }
+
+    /**
+     * 提交用户信息，完成注册
+     * @param strNumber
+     * @param strName
+     * @param strPwd
+     */
+    private void submitUserInfo(String strNumber, String strName, String strPwd) {
+        String address = "";    //服务器地址
+
+        //拼接用户信息
+        String userInfo = "user_number=" + strNumber + "&user_name=" + strName
+                            + "&user_pwd" + strPwd;
+
+        //将信息上传服务器，并接受返回的信息
+        String ret_code = HttpUtil.sendPost(address, userInfo);
+
+        //对服务器返回的信息进行判断
+        if (ret_code == null){
+            //如果返回的ret_code为null，提示用户注册失败
+            handler.sendEmptyMessage(CODE_FAILE);
+        }
+
+        try{
+            //对返回的信息进行JSON解析
+            JSONObject result_json = new JSONObject(ret_code);
+            int result_code = result_json.getInt("ret_code");
+
+            if (result_code == CODE_SUCCESS){
+                //提示用户注册成功，并跳转
+                handler.sendEmptyMessage(CODE_SUCCESS);
+            }else if (result_code == CODE_FAILE){
+                handler.sendEmptyMessage(CODE_FAILE);
+            }else {
+                handler.sendEmptyMessage(CODE_NOTALL);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.what){
+                case 0:
+                    //注册成功，实现跳转
+                    Intent intent = new Intent(UserRegister.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+                case 1:
+                    Toast.makeText(UserRegister.this, "注册失败，请重新尝试", Toast.LENGTH_SHORT).show();
+                    break;
+                case 2:
+                    Toast.makeText(UserRegister.this, "请输入完整的注册信息", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
 
     private void initView() {
         mToolBar = (Toolbar) findViewById(R.id.regis_tool);
